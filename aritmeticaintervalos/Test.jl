3547e35155047f7e887c83d84484008e1110953d
lauraperezgarcia <laup@ciencias.unam.mx>
2015-04-13 00:39:40 -0500

    pruebaas para intervalos

--- aritmeticaintervalos/Test.jl
+++ aritmeticaintervalos/Test.jl
@@ -3,44 +3,39 @@ using Intervalos
 using FactCheck
 
 
-facts("primeros facts que voy a realizar") do
-
-@fact 1=>1
-
-@fact 2*2 => 4
-
-@fact Intervalo(0)+Intervalo(1,0)=>Intervalo(1,0)
-
-@fact 3*Intervalo(1,0)=>Intervalo(3,0)
+facts("definición correcta de intervalos, prop elementales") do
 
 @fact (0.1 ∈ [0.1, 0.2])=>true
 @fact (0.1 ∈ Intervalo(0, 0.2)) => true
-#que sea capaz de voltear un intervalo si se encuentra al revés
-
 @fact Intervalo(0,-1)=>Intervalo(-1, 0)
-
-
-#ALGUNAS PROPIEDADES DE LA DIVISIÓN ENTRE INTERVALOS QUE CONTIENEN AL CERO
-@fact Intervalo(1,1)/Intervalo(-1,Inf)=>UnionI(Intervalo(-Inf,-1),Intervalo(0,Inf))
-
-##que sea capaz de mostrar que un intervalo se encuentra en otro mas grande
-
 @fact (0 ∈ UnionI(Intervalo(-9, -1), Intervalo(1,Inf)))=> false
-
 @fact (Intervalo(-1,1) ⊂ Intervalo(-2,2)) =>true
-
-
 @fact (Intervalo(-1,1) ⊂ Intervalo(0,10)) =>false
-
-
 @fact (Intervalo(-10,10) ⊂ Intervalo(-2,2)) =>false
 
+
+end
+
+
+facts("sumas, restas, multiplicaciones, divisiones") do
+
+@fact Intervalo(-9, 1)+Intervalo(6,10)=>Intervalo(-3, 11)
+@fact Intervalo(0)+Intervalo(1,0)=>Intervalo(1,0) #suma
+@fact Intervalo(0)-Intervalo(1,0)=>Intervalo(-1,0)   #resta
+@fact Intervalo(1)*-1-(-2*Intervalo(2,0))=> Intervalo(-1,3)
+@fact 3*Intervalo(1,0)=>Intervalo(3,0)
+@fact Intervalo(1,1)/Intervalo(-1,Inf)=>UnionI(Intervalo(-Inf,-1),Intervalo(0,Inf))
 @fact Intervalo(-1,1)^2=> Intervalo(0,1)
 @fact Intervalo(-1,1)*Intervalo(-1,1)=> Intervalo(-1,1)
 
-#@fact eva(cos, Intervalo)
-@fact eva(cos, Intervalo(0, π)) => Intervalo(-1,1)
+end
+
+facts("funciones elementales")do
+###para funciones monótonas ^, exp
+#@fact log(10, Intervalo(1,2))=>Intervalo(log(1,10), log(2 ,10)) 
 @fact Intervalo(0,4)^0.5=>Intervalo(0, 2)
+
+@fact exp(Intervalo(0.2, 0.4))=>Intervalo(RDOWN(exp,0.2), RUP(exp,0.4))
 
 ###checar cosas de raices
 end
